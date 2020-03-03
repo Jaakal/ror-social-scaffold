34c225ca2a7c3fe6584e42062327428d2a8cf8b9
Jaakal <jaak.kivinukk@gmail.com>
2020-03-03 22:02:59 +0200

    Tweaked the inverse relationship creation, created tests and readme file

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,15 +1,11 @@
 class Friendship < ApplicationRecord
-  after_create :create_inverse_friendship
+  after_update :create_inverse_friendship
   belongs_to :sender, foreign_key: :friendable_id, class_name: 'User'
   belongs_to :reciver, foreign_key: :friend_id, class_name: 'User'
 
   private
 
-  def create_inverse_friendship   
-    reciver.friendships.create(friend_id: sender.id)  if !already_friends?
-  end
-
-  def already_friends?
-    reciver.friendships.exists?(friend_id: sender.id)
+  def create_inverse_friendship
+    reciver.friendships.create(friend_id: sender.id, status: true)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,8 +9,8 @@ class User < ApplicationRecord
   has_many :posts
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
-  has_many :friendships, foreign_key: :friendable_id, class_name: "Friendship"
-  has_many :inverse_friendships, foreign_key: :friend_id, class_name: "Friendship"
+  has_many :friendships, foreign_key: :friendable_id, class_name: 'Friendship'
+  has_many :inverse_friendships, foreign_key: :friend_id, class_name: 'Friendship'
 
   # has_many :recived_requests, foreign_key: :friendable_id, class_name: 'Friendship'
   # has_many :sent_requests, foreign_key: :friend_id, class_name: 'Friendship'
