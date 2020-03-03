fcc667e8b158669757963ed278a6c0993fc7e147
Yair Facio <yair.facio11@gmail.com>
2020-03-03 13:05:12 -0600

    Create inverse friendship callback

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,4 +1,15 @@
 class Friendship < ApplicationRecord
+  after_create :create_inverse_friendship
   belongs_to :sender, foreign_key: :friendable_id, class_name: 'User'
   belongs_to :reciver, foreign_key: :friend_id, class_name: 'User'
+
+  private
+
+  def create_inverse_friendship   
+    reciver.friendships.create(friend_id: sender.id)  if !already_friends?
+  end
+
+  def already_friends?
+    reciver.friendships.exists?(friend_id: sender.id)
+  end
 end
